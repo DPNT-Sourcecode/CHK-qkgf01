a547d2a91e76cf1f0f7fb924dcb461d551d9dd40
sbx_user1051 <sbx_user1051@169.254.94.165>
2025-03-12 17:27:56 +0000

    Wed Mar 12 17:27:56 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    x = 13
+    y = 65
+
+    sum = x + y 
+    print("The sum of x and y is " + sum)
+
