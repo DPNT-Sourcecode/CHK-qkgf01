c99970bddcb059734da1875555f50bd3c55b770a
sbx_user1051 <sbx_user1051@169.254.94.165>
2025-03-12 17:50:11 +0000

    Wed Mar 12 17:50:11 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,12 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    # This is for validating the input
+    if not isinstance(skus, str):
+        return -1
+
+    items = {'A','B','C','D'}
+    
+     
+
